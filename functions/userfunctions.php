18dcaf9dacdbb5c45794d6aa8d4734f3b6caa2fe
Mehta Mihir Jayeshkumar <mehtamihir2304@gmail.com>
2024-07-23 15:46:44 +0530

    Add files via upload

---
+++ functions/userfunctions.php
@@ -0,0 +1,17 @@
+<?php
+
+include('config/config.php');
+
+function getAllActive($table)
+{
+    global $con;
+    $query = "SELECT * FROM $table WHERE status='0' ";
+    return $query_run = mysqli_query($con, $query);
+}
+
+function redirect($url, $message)
+{
+    $_SESSION['message'] = $message;
+    header('Location: '.$url);
+    exit();
+}
